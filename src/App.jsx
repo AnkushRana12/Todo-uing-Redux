a7e6c388e3cd5f0fd5858864b344cff0eb4fc475
AnkushRana12 <ajajsiw1234@gmail.com>
2025-12-16 15:32:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+const App = () => {
+
+  return (
+
+    <div>
+      <h1>ANKUSH RANA</h1>
+      <AddTodo/>
+      <Todos/>
+    </div>
+  )
+}
+
+export default App
